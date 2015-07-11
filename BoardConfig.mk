81896b7a962a12ed7c2e2723c3b8487ebb1c44b1
W4TCH0UT <ateekujjawal@gmail.com>
2015-07-11 09:25:50 +0530

    ghost: enable LZMA compression/decompression technique
    Change-Id: I5eb7b04304c46d72fd587326f4a1baf7892ca9ad
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@ LOCAL_PATH := device/motorola/ghost
 
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-6.0/bin
 
+# Zipping options
+WITH_LZMA_OTA := true
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := xt1052,ghost,xt1053,ghost_retail,xt1055,ghost_usc,xt1056,ghost_sprint,xt1058,ghost_att,ghost_rcica,xt1060,ghost_verizon
 
